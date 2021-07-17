25eb1b148f77a6a1b2cd6075d8d38b954fbbfb6d
hefeng6500 <2443992009@qq.com>
2021-07-17 17:40:46 +0800

    feat: pack scss use glup

---
+++ packages/theme-chalk/gulpfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const { series, src, dest } = require("gulp");
+const sass = require("gulp-dart-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssmin = require("gulp-cssmin");
+const rename = require("gulp-rename");
+
+const noElPrefixFile = /(index|base|display)/;
+
+function compile() {
+  return src("./src/*.scss")
+    .pipe(sass.sync())
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cssmin())
+    .pipe(
+      rename(function (path) {
+        if (!noElPrefixFile.test(path.basename)) {
+          path.basename = `h-${path.basename}`;
+        }
+      })
+    )
+    .pipe(dest("./lib"));
+}
+
+function copyfont() {
+  return src("./src/fonts/**").pipe(cssmin()).pipe(dest("./lib/fonts"));
+}
+
+exports.build = series(compile, copyfont);
